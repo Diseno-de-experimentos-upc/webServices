902cc0a54c9a1e939dba4581ee7f79e70d7332e0
Abel Cierto <83315281+AbelCierto@users.noreply.github.com>
2022-10-28 23:31:29 -0500

    Initial Commit: Created the file to start the implementation.

---
+++ src/main/java/com/example/digitalmindwebservices/DigitalMindWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.digitalmindwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalMindWebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalMindWebServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/digitalmindwebservices/DigitalMindWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalmindwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalMindWebServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
