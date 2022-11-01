736f62b90bce81318f9a2bb85ca8d51638e49c72
Abel Cierto <83315281+AbelCierto@users.noreply.github.com>
2022-11-01 12:01:24 -0500

    feat(controller): Finished the testing user controller

--- src/test/java/com/example/digitalmindwebservices/controller/UserControllerTest.java
+++ src/test/java/com/example/digitalmindwebservices/controller/UserControllerTest.java
@@ -2,20 +2,23 @@ package com.example.digitalmindwebservices.controller;
 
 import com.example.digitalmindwebservices.entities.User;
 import com.example.digitalmindwebservices.service.impl.UserServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = UserController.class)
@@ -67,5 +70,60 @@ public class UserControllerTest {
                 "Perez", "juan@gmail.com", "993293832","987987987", "developer" ," I am a user of the system");
         given(userService.getById(id)).willReturn(Optional.of(user));
         mockMvc.perform(get("/api/users/{id}", id)).andExpect(status().isOk());
+    }
+
+    @Test
+    void findUserByEmail() throws Exception {
+        String email = "juan@gmail.com";
+        User user = new User(1L, "Juan",
+                "Perez", "juansito@gmail.com", "juan#123","987987987", "developer" ," I am a user of the system");
+        given(userService.findByEmail(email)).willReturn(user);
+        mockMvc.perform(get("/api/users/searchByEmail/{email}", email)).andExpect(status().isOk());
+    }
+
+    @Test
+    void findUserByFirstName() throws Exception {
+        String firstName = "Luis";
+        given(userService.findByFirstName(firstName)).willReturn(userList.stream()
+                .filter(u ->u.getFirstName().equals(firstName)).collect(Collectors.toList()));
+        mockMvc.perform(get("/api/users/searchByFirstName/{firstName}", firstName)).andExpect(status().isOk());
+    }
+    @Test
+    void insertUserTest() throws Exception {
+        User user = new User(1L, "Juan",
+                "Perez", "juan@gmail.com", "Juan#Az", "987987987", "developer" ," I am a user of the system");
+        mockMvc.perform(post("/api/users")
+                .content(asJsonString(user))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void updateUserTest() throws Exception {
+        Long id = 1L;
+        User user = new User(1L, "Juan",
+                "Perez", "juanito@gmail.com", "Juan#Az", "987987987", "developer", " I am a user of the system");
+        given(userService.getById(id)).willReturn(Optional.of(user));
+        mockMvc.perform(put("/api/users/{id}", id)
+                .content(asJsonString(user))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
+    }
+    @Test
+    void deleteUserTest() throws Exception {
+        Long id = 1L;
+        User user = new User(1L, "Juan",
+                "Perez", "juan@gmail.com", "Juan#Az", "987987987", "developer", " I am a user of the system");
+        given(userService.getById(id)).willReturn(Optional.of(user));
+        mockMvc.perform(delete("/api/users/{id}", id)).andExpect(status().isOk());
+    }
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
