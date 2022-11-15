2ba1afdb67f8f4422fa6d4716667308b75e7a617
MICHAELCARRILLO <u20201a924@upc.edu.pe>
2022-11-14 19:57:15 -0500

    feat(companies): Added companies service interface .

---
+++ src/main/java/com/example/digitalmindwebservices/service/ICompanyService.java
@@ -0,0 +1,7 @@
+package com.example.digitalmindwebservices.service;
+
+import com.example.digitalmindwebservices.entities.Company;
+
+public interface ICompanyService extends CrudService<Company> {
+}
+
